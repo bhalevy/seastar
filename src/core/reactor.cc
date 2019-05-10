8167b68cc7b1ccc3b9061f62d32a00cff99ef390
Rafael Ávila de Espíndola <espindola@scylladb.com>
2019-05-10 16:10:42 -0700

    make report_failed_future noexcept

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -5649,7 +5649,7 @@ void engine_exit(std::exception_ptr eptr) {
     engine().exit(1);
 }
 
-void report_failed_future(const std::exception_ptr &eptr) {
+void report_failed_future(const std::exception_ptr &eptr) noexcept {
     seastar_logger.warn("Exceptional future ignored: {}, backtrace: {}", eptr, current_backtrace());
 }
 
