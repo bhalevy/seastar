b82fca6928dafd9c1a1a9d109ab2db12ee1b8812
Benny Halevy <bhalevy@scylladb.com>
2019-09-10 17:12:08 +0300

    tests: test_fstream_unaligned: close file
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- tests/unit/fstream_test.cc
+++ tests/unit/fstream_test.cc
@@ -197,6 +197,8 @@ SEASTAR_TEST_CASE(test_fstream_unaligned) {
                     // assert that file was indeed truncated to the amount of bytes written.
                     BOOST_REQUIRE(size == 40);
                     return make_ready_future<>();
+                }).finally([f] () mutable {
+                    return f.close().then([f] {});
                 });
             });
         }).then([] {
