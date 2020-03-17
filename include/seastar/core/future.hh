3f960b4bf5ed82abd8f76052b3893c79c9bd0cd0
Benny Halevy <bhalevy@scylladb.com>
2020-03-17 11:29:47 +0200

    future: rethrow_with_nested: make noexcept
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- include/seastar/core/future.hh
+++ include/seastar/core/future.hh
@@ -1015,7 +1015,7 @@ private:
     }
 
     [[gnu::noinline]]
-    future<T...> rethrow_with_nested() {
+    future<T...> rethrow_with_nested() noexcept {
         if (!failed()) {
             return internal::current_exception_as_future<T...>();
         } else {
@@ -1030,7 +1030,11 @@ private:
             try {
                 get();
             } catch (...) {
-                std::throw_with_nested(f_ex);
+                try {
+                    std::throw_with_nested(f_ex);
+                } catch (...) {
+                    return internal::current_exception_as_future<T...>();
+                }
             }
             __builtin_unreachable();
         }
