4676ecb0d18faadb7b1c0b8bb580cfd74643135a
Benny Halevy <bhalevy@scylladb.com>
2020-05-04 20:18:49 +0300

    gate: make with_gate noexcept
    Define gate::with_gate that returns an exception
    future rather than throwing if the gate is closed.
    
    Call gate::with_gate from with_gate in SEASTAR_API_LEVEL 3.
    
    Note that this is an API change. If someone is trying to
    catch a gate_closed_exception they will now have to look
    at the future.  This behavior is kept in SEASTAR_API_LEVEL 2.
    
    Add test_with_gate unit test to test both API versions;
    but don't bump the API version just yet, this will be done
    in the next patch.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- include/seastar/core/gate.hh
+++ include/seastar/core/gate.hh
@@ -25,6 +25,8 @@
 #include <seastar/util/std-compat.hh>
 #include <exception>
 
+#include <seastar/core/internal/api-level.hh>
+
 namespace seastar {
 
 /// \addtogroup fiber-module
@@ -105,10 +107,53 @@ public:
     bool is_closed() const {
         return bool(_stopped);
     }
+
+    /// Invokes the function \c func making sure the gate \c g is properly entered
+    /// and later on, properly left.
+    ///
+    /// If the gate is already closed, an exception future holding
+    /// \ref gate_closed_exception is returned.
+    template <typename Func>
+    auto with_gate(Func&& func) noexcept {
+        using futurator = futurize<std::result_of_t<Func()>>;
+        if (is_closed()) {
+            return futurator::make_exception_future(gate_closed_exception());
+        }
+        ++_count;
+        return futurator::invoke(std::forward<Func>(func)).finally([this] { leave(); });
+    }
 };
+
+namespace api_v2 {
 
 /// Executes the function \c func making sure the gate \c g is properly entered
 /// and later on, properly left.
+///
+/// If the gate is already closed, a \ref gate_closed_exception is thrown.
+///
+/// \param func function to be executed
+/// \param g the gate. Caller must make sure that it outlives this function.
+/// \returns whatever \c func returns
+///
+/// \relates gate
+template <typename Func>
+[[deprecated("Instead, use Seastar_API_LEVEL 3 or higher for with_gate noexcept")]]
+inline
+auto
+with_gate(gate& g, Func&& func) {
+    g.enter();
+    return futurize_invoke(std::forward<Func>(func)).finally([&g] { g.leave(); });
+}
+
+} // namespace api_v2
+
+namespace api_v3::and_newer {
+
+/// Invokes the function \c func making sure the gate \c g is properly entered
+/// and later on, properly left.
+///
+/// If the gate is already closed, an exception future holding
+/// \ref gate_closed_exception is returned.
 ///
 /// \param func function to be executed
 /// \param g the gate. Caller must make sure that it outlives this function.
@@ -118,10 +163,11 @@ public:
 template <typename Func>
 inline
 auto
-with_gate(gate& g, Func&& func) {
-    g.enter();
-    return futurize_invoke(std::forward<Func>(func)).finally([&g] { g.leave(); });
+with_gate(gate& g, Func&& func) noexcept {
+    return g.with_gate(std::forward<Func>(func));
 }
+
+} // namespace api_v3
 /// @}
 
 }
--- tests/unit/futures_test.cc
+++ tests/unit/futures_test.cc
@@ -31,12 +31,15 @@
 #include <seastar/core/manual_clock.hh>
 #include <seastar/core/thread.hh>
 #include <seastar/core/print.hh>
+#include <seastar/core/gate.hh>
 #include <seastar/util/log.hh>
 #include <boost/iterator/counting_iterator.hpp>
 #include <seastar/testing/thread_test_case.hh>
 
 #include <boost/range/iterator_range.hpp>
 #include <boost/range/irange.hpp>
+
+#include <seastar/core/internal/api-level.hh>
 
 using namespace seastar;
 using namespace std::chrono_literals;
@@ -1228,4 +1231,51 @@ SEASTAR_TEST_CASE(test_warn_on_broken_promise_with_no_future) {
     (void)p.get_future();
     p.set_exception(std::runtime_error("foo"));
     return make_ready_future<>();
+}
+
+SEASTAR_THREAD_TEST_CASE(test_with_gate) {
+    gate g;
+    int counter = 0;
+    int gate_closed_errors = 0;
+    int other_errors = 0;
+
+    BOOST_CHECK_NO_THROW(with_gate(g, [&] { counter++; }).get());
+    BOOST_REQUIRE_EQUAL(counter, 1);
+
+    g.close().get();
+
+    BOOST_CHECK_THROW(with_gate(g, [&] { counter++; }).get(), gate_closed_exception);
+    BOOST_REQUIRE_EQUAL(counter, 1);
+
+    BOOST_CHECK_THROW(api_v2::with_gate(g, [&] {
+            counter++;
+        }).then_wrapped([&] (future<> f) {
+            auto eptr = f.get_exception();
+            try {
+                std::rethrow_exception(eptr);
+            } catch (const gate_closed_exception& e) {
+                gate_closed_errors++;
+            } catch (...) {
+                other_errors++;
+            }
+        }).get(), gate_closed_exception);
+    BOOST_REQUIRE_EQUAL(counter, 1);
+    BOOST_REQUIRE_EQUAL(gate_closed_errors, 0);
+    BOOST_REQUIRE_EQUAL(other_errors, 0);
+
+    BOOST_CHECK_NO_THROW(api_v3::with_gate(g, [&] {
+            counter++;
+        }).then_wrapped([&] (future<> f) {
+            auto eptr = f.get_exception();
+            try {
+                std::rethrow_exception(eptr);
+            } catch (const gate_closed_exception& e) {
+                gate_closed_errors++;
+            } catch (...) {
+                other_errors++;
+            }
+        }).get());
+    BOOST_REQUIRE_EQUAL(counter, 1);
+    BOOST_REQUIRE_EQUAL(gate_closed_errors, 1);
+    BOOST_REQUIRE_EQUAL(other_errors, 0);
 }
