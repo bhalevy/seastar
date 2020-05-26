d020dc9c01d3bc2e3c082d02a1a4879c3f2103a4
Benny Halevy <bhalevy@scylladb.com>
2020-05-26 14:03:57 +0300

    file: add with_file_close_on_error helper
    Auto-closes a file passed to the `func` parameter if
    `func` returns an exception.  If close() fails too,
    it's exception will be returned as a nested exception of
    the outer one.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- demos/file_demo.cc
+++ demos/file_demo.cc
@@ -31,6 +31,7 @@
 
 #include <seastar/core/aligned_buffer.hh>
 #include <seastar/core/file.hh>
+#include <seastar/core/fstream.hh>
 #include <seastar/core/seastar.hh>
 #include <seastar/core/sstring.hh>
 #include <seastar/core/temporary_buffer.hh>
@@ -87,11 +88,22 @@ int main(int ac, char** av) {
             std::cout << "writing random data into " << data_filename << std::endl;
             std::generate(wbuf.get_write(), wbuf.get_write() + aligned_size, [&dist, &rnd] { return dist(rnd); });
 
-            // `with_file` is used to create/open `data_filename` just around the call to `dma_write`
-            count = with_file(open_file_dma(data_filename, open_flags::rw | open_flags::create), [&wbuf] (file& f) {
-                return f.dma_write(0, wbuf.get(), aligned_size);
+            // with_file_close_on_failure will close the opened `data_filename` file only if
+            // `make_file_output_stream` returns an error. Otherwise, in the error-free path,
+            // the opened file is moved to `file_output_stream` that in-turn closes it
+            // when the stream is closed.
+            output_stream<char> o = with_file_close_on_failure(open_file_dma(data_filename, open_flags::rw | open_flags::create), [] (file f) {
+                return make_file_output_stream(std::move(f), aligned_size);
             }).get0();
-            assert(count == aligned_size);
+            auto deferred_close = defer([&o] { return o.close(); });
+
+            // one byte at a time, to demonstrate output stream
+            for (size_t i = 0; i < aligned_size; i++) {
+                char c = wbuf[i];
+                o.write(&c, 1);
+            }
+            deferred_close.cancel();
+            o.close().get();
 
             // verify the data via meta_filename
             std::cout << "verifying data..." << std::endl;
