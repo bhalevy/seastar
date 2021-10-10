6ac5e259dc7ebea2b24a0eaf14f6d3daea6cf011
Benny Halevy <bhalevy@scylladb.com>
2021-10-10 11:24:53 +0300

    queue: mark not_empty and not_full noexcept
    Now that empty()/full() are marked noexcept,
    they never throw from any other path (promise
    default constructor doesn't throw either).
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- include/seastar/core/queue.hh
+++ include/seastar/core/queue.hh
@@ -76,11 +76,11 @@ public:
     /// Returns a future<> that becomes available when pop() or consume()
     /// can be called.
     /// A consumer-side operation. Cannot be called concurrently with other consumer-side operations.
-    future<> not_empty();
+    future<> not_empty() noexcept;
 
     /// Returns a future<> that becomes available when push() can be called.
     /// A producer-side operation. Cannot be called concurrently with other producer-side operations.
-    future<> not_full();
+    future<> not_full() noexcept;
 
     /// Pops element now or when there is some. Returns a future that becomes
     /// available when some element is available.
@@ -269,7 +269,7 @@ bool queue<T>::full() const noexcept {
 
 template <typename T>
 inline
-future<> queue<T>::not_empty() {
+future<> queue<T>::not_empty() noexcept {
     if (_ex) {
         return make_exception_future<>(_ex);
     }
@@ -283,7 +283,7 @@ future<> queue<T>::not_empty() {
 
 template <typename T>
 inline
-future<> queue<T>::not_full() {
+future<> queue<T>::not_full() noexcept {
     if (_ex) {
         return make_exception_future<>(_ex);
     }
