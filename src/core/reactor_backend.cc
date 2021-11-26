bhalevy/seastar

commit 528762adbaf2c6dde857957b920606378959e8fb

aio_general_context: flush: handle io_submit short return
According to io_submit(2):
    On success, io_submit() returns the number of iocbs submitted
    (which may be less than nr, or 0 if nr is zero).

Currently we assume that we're able to submit all iocb's
on success, and adjust `last` accordingly.

If io_submit returns a positive return value that
is less than the number requested, we will lose
all iocb's that weren't submitted.

Instead, continue submitting more iocbs until done.

It is possible that with a shortage of kernel resources,
io_submit will fail with EAGAIN.
Handle this case as well, requiring that some progress
will be made between consecutive need_preempt() events.

Fixes #921

Test: unit(dev)

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
Message-Id: <20210715090318.237360-1-bhalevy@scylladb.com>

src/core/reactor_backend.cc
@@ -271,14 +271,25 @@ void aio_general_context::queue(linux_abi::iocb* iocb) {
 }
 
 size_t aio_general_context::flush() {
-    if (last != iocbs.get()) {
-        auto nr = last - iocbs.get();
-        last = iocbs.get();
-        auto r = io_submit(io_context, nr, iocbs.get());
-        assert(r >= 0);
-        return nr;
+    auto begin = iocbs.get();
+    auto retried = last;
+    while (begin != last) {
+        auto r = io_submit(io_context, last - begin, begin);
+        if (__builtin_expect(r > 0, true)) {
+            begin += r;
+            continue;
+        }
+        // errno == EAGAIN is expected here. We don't explicitly assert that
+        // since the assert below requires that some progress will be
+        // made, preventing an endless loop for any reason.
+        if (need_preempt()) {
+            assert(retried != begin);
+            retried = begin;
+        }
     }
-    return 0;
+    auto nr = last - iocbs.get();
+    last = iocbs.get();
+    return nr;
 }
 
 completion_with_iocb::completion_with_iocb(int fd, int events, void* user_data)

src/core/reactor_backend.hh
@@ -53,6 +53,7 @@ struct aio_general_context {
     internal::linux_abi::iocb** last;
     internal::linux_abi::iocb** const end;
     void queue(internal::linux_abi::iocb* iocb);
+    // submit all queued iocbs and return their count.
     size_t flush();
 };
 

commit c98f84d99b5594065882b2ef4b2f00762ed1ca55

aio_general_context: flush: provide 1 second grace for retries
The existing assert that was introduced in
528762adbaf2c6dde857957b920606378959e8fb
is too harsh.  It will fail on the first retry
failure after `needs_preeempt` returned true.

This lead to the following crash:
```
scylla: ./seastar/src/core/reactor_backend.cc:286: size_t seastar::aio_general_context::flush(): Assertion `retried != begin' failed.
```

Decoded backtrace:
```
seastar::aio_general_context::flush() at ./build/release/seastar/./seastar/src/core/reactor_backend.cc:286
 (inlined by) seastar::reactor_backend_aio::wait_and_process_events(__sigset_t const*) at ./build/release/seastar/./seastar/src/core/reactor_backend.cc:489
seastar::reactor::sleep() at ./build/release/seastar/./seastar/src/core/reactor.cc:3041
 (inlined by) seastar::reactor::do_run() at ./build/release/seastar/./seastar/src/core/reactor.cc:3007
seastar::reactor::run() at ./build/release/seastar/./seastar/src/core/reactor.cc:2839
seastar::app_template::run_deprecated(int, char**, std::function&&) at ./build/release/seastar/./seastar/src/core/app-template.cc:228
seastar::app_template::run(int, char**, std::function ()>&&) at ./build/release/seastar/./seastar/src/core/app-template.cc:128
main at ./main.cc:492
```

This change uses the lowres_clock to allow flush to complete
within 1 second of the first `io_submit` failure.

Fixes #975

Test: unit(release)

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

src/core/reactor_backend.cc
@@ -26,6 +26,8 @@
 #include <seastar/core/internal/buffer_allocator.hh>
 #include <seastar/util/defer.hh>
 #include <seastar/util/read_first_line.hh>
+#include <seastar/core/lowres_clock.hh>
+
 #include <chrono>
 #include <sys/poll.h>
 #include <sys/syscall.h>
@@ -272,7 +274,8 @@ void aio_general_context::queue(linux_abi::iocb* iocb) {
 
 size_t aio_general_context::flush() {
     auto begin = iocbs.get();
-    auto retried = last;
+    constexpr lowres_clock::time_point no_time_point = lowres_clock::time_point(lowres_clock::duration(0));
+    auto retry_until = no_time_point;
     while (begin != last) {
         auto r = io_submit(io_context, last - begin, begin);
         if (__builtin_expect(r > 0, true)) {
@@ -280,11 +283,12 @@ size_t aio_general_context::flush() {
             continue;
         }
         // errno == EAGAIN is expected here. We don't explicitly assert that
-        // since the assert below requires that some progress will be
-        // made, preventing an endless loop for any reason.
-        if (need_preempt()) {
-            assert(retried != begin);
-            retried = begin;
+        // since the assert below prevents an endless loop for any reason.
+        if (retry_until == no_time_point) {
+            // allow retrying for 1 second
+            retry_until = lowres_clock::now() + 1s;
+        } else {
+            assert(lowres_clock::now() < retry_until);
         }
     }
     auto nr = last - iocbs.get();